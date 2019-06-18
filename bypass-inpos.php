df3cbc92d16d0454a14f2b2fb32778a24c4f45c4
mhmdbgs1337 <47492362+mhmdbgs1337@users.noreply.github.com>
2019-06-18 16:44:31 +0700

    Create bypass-inpos.php

---
+++ bypass-inpos.php
@@ -0,0 +1,80 @@
+<?php
+	/*
+		SQLInjection Vuln Checker
+		Coded by João Artur (K3N1)
+		Designed by ZanutSec
+		http://www.youtube.com/c/KeniGamer
+		http://www.github.com/JoaoArtur
+		https://www.youtube.com/user/hackzanut
+		https://twitter.com/zanutsec
+	*/
+	?>
+<html>
+<head>
+	<title>Bypass Internet Positif</title>
+	<meta name="description" content="Bypass Internet Positif">
+	<link href="https://fonts.googleapis.com/css?family=Dancing+Script|Indie+Flower|Kalam|Merienda&display=swap" rel="stylesheet">
+</head>
+<style>
+body{
+	font-family: "Racing Sans One", cursive;
+	background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII=");
+	color:white;
+	background-attachment:fixed;
+	background-repeat:no-repeat;
+	background-position:relative;
+	background-color:transparan;
+	-webkit-background-size: 100% 100%;
+}
+#content tr:hover{
+background-color: transparan;
+text-shadow:0px 0px 10px #fff;
+}
+#content .first{
+background-color: transparan;
+}
+table{
+border: 1px #000000 dotted;
+}
+a{
+color:transparan;
+text-decoration: none;
+}
+a:hover{
+color:transparan;
+text-shadow:0px 0px 10px #ffffff;
+}
+input,select,textarea{
+border: 1px #000000 solid;
+-moz-border-radius: 5px;
+-webkit-border-radius:5px;
+border-radius:5px;
+}
+</style>
+</head>
+<body>
+	<center>
+		<h1><font face="Merienda" size="7" color="black">Bypass Internet Positif</font></h1>
+		<br>
+		<font face="Kalam" size="5" color="black">Coded by </font><font color="red" face="Kalam" size="5">HatsuSec</font>
+		<br>
+<form action="#" method="get">
+ <input type="hidden" name="nama" value="https://googleweblight.com/i?u="></input><br/>
+ <input type="text" name="alamat" placeholder="http://webshit.co.li/" size="25"></input><br/>
+ <input type="submit" name="submit" value="Submit"></input>
+</form>
+</body>
+</html>
+<?php
+if( $_GET["nama"] || $_GET["alamat"])
+{
+ echo "<a href='". $_GET['nama']. "". $_GET["alamat"]. "'><br><font color='green' size='3'>Click Me Senpai</font></br></a></center>";
+}
+
+?>
+<html>
+<center>
+<br><br>
+<font face="Kalam" size="5" color="red">Nb : Gunakan http:// atau https:// supaya tidak error</font>
+</center>
+</html>
